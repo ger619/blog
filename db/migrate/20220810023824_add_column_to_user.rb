a2d16e265f3faf7bda3ad2ef124dc304ae6a59aa
David Abol Ger <draker74@gmail.com>
2022-08-10 10:11:24 +0300

    snake_case changes

--- db/migrate/20220810023824_add_column_to_user.rb
+++ db/migrate/20220810023824_add_column_to_user.rb
@@ -1,5 +1,6 @@
 class AddColumnToUser < ActiveRecord::Migration[7.0]
   def change
+    add_column :users, :author_id, :integer
     add_column :users, :name, :string
     add_column :users, :photo, :string
     add_column :users, :bio, :string
