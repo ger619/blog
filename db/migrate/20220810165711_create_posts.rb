4b5a6ff96f73fd29b2ff9d7f4698ee77479ba877
David Abol Ger <draker74@gmail.com>
2022-08-11 06:38:52 +0300

    Association without linters

--- db/migrate/20220810165711_create_posts.rb
+++ db/migrate/20220810165711_create_posts.rb
@@ -1,12 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.references :author, foreign_key: { to_table: :users }
-      t.integer :author_id, add_reference: user ,foreign_key: { to_table: :users }
+      t.references :author, null:false, foreign_key: { to_table: :users } # author_id
       t.string :title
       t.string :text
       t.integer :comment_counter
-      t.integer :likes_counter,
+      t.integer :likes_counter
 
       t.timestamps
     end
--- db/migrate/20220811033434_create_comments.rb
+++ db/migrate/20220811033434_create_comments.rb
@@ -1,8 +1,9 @@
-class CreateLikes < ActiveRecord::Migration[7.0]
+class CreateComments < ActiveRecord::Migration[7.0]
   def change
-    create_table :likes do |t|
+    create_table :comments do |t|
       t.references :user, null:false, foreign_key: { to_table: :users } # user_id
       t.references :post, null:false, foreign_key: { to_table: :posts } # post_id
+      t.string :text
 
       t.timestamps
     end
