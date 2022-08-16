770f579d6c4300fd105cf35802f825ece5ae20b0
David Abol Ger <draker74@gmail.com>
2022-08-17 00:58:02 +0300

    Controller specs

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -6,5 +6,9 @@ RSpec.describe 'Posts', type: :request do
       get '/user/:user_id/'
       expect(response).to have_http_status(:success)
     end
+    it 'renders the index template' do
+      get '/user/:user_id/'
+      expect(response).to render_template(:show)
+    end
   end
 end
--- spec/views/user/user.html.erb_spec.rb
+++ spec/views/user/user.html.erb_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'user/show.html.erb', type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  # it 'renders the index template' do
+  before(:all) { get user_index_path }
+  it 'renders the index template' do
+    expect(response).to render_template('index')
+  end
 end
