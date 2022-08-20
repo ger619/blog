91bcef9889f098ec07a3e22cbc06ea1878812f9a
David Abol Ger <draker74@gmail.com>
2022-08-20 21:03:12 +0300

    Adding forms

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,14 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250 }
   validates :comment_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
   validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
-  belongs_to :user
+
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   has_many :comments
   has_many :likes
 
   def post_update
     # A method that updates the posts counter for a user.
-    user.increment!(:posts_counter)
+    user.update(posts_counter: user.post.count)
   end
 
   def recent_comment
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "user#index"
   resources :user, only: [:index, :show] do
-    resources :post, only: [:index, :show]
+    resources :post, only: [:index, :show, :new, :create] do
+      resources :comment, only: [:create]
+      resources :like, only: [:create]
+    end
   end
 end
